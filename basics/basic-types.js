fb1b1cac2a274fd24946780b53490e87782f96d5
GOKUL KANNAMBALLY BALACHNADRAN <52125327+gokintosh@users.noreply.github.com>
2021-03-05 14:17:28 +0100

    1

---
+++ basics/basic-types.js
@@ -0,0 +1,18 @@
+module.exports.isACat=(animal)=>animal==="cat";
+
+function addNumbers(a,b){
+    return a+b;
+}
+
+
+module.exports.addNumbers=addNumbers;
+
+const introduction=(name)=>`Hello ${name}`;
+
+module.exports.introduction=introduction;
+
+module.exports.introducePerson=(name)=>console.log(introduction(name));
+
+module.exports.dumpObject=(type,object)=>`${type}:${JSON.stringify(obj)}`;
+
+module.exports.dumpStringOrNumber=(info)=>`value ${info}`;
